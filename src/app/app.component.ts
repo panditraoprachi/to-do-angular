9736f1056b12140210bc3e387cc806067079bc2a
“PRACHI_KULKARNI” <panditraoprachi@gmail.com>
2023-12-01 10:23:44 -0500

    adding to do list commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do-list';
+  arrNew:any[]=[];
+  addTask(task:any){
+    this.arrNew.push({id:this.arrNew.length,name:task});
+    console.log(this.arrNew);
+  }
+  deleteTask(id:number){
+    this.arrNew=this.arrNew.filter(index=>index.id!==id);
+  }
+  deleteAll(){
+    this.arrNew=[];
+  }
+  // data=[{task:'Wash Laundry'},
+  //       {task:'Take a walk with dog'}]
 }
